355769a504590baa702b95205628dd159dd3ba2e
Ryota Misawa <ideodora@gmail.com>
2023-08-25 14:56:24 +0000

    create sveltekit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let hoge = 'hoge';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2 class="mx-4 mt-3 bg-red-500 p-4 text-red-500">hello {hoge}</h2>
